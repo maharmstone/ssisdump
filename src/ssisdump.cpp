544f79de681b0ebe8e971ce945a9b703c66cbe96
Mark Harmstone <mark@harmstone.com>
2022-05-25 16:09:01 +0100

    dump_ssis: put files into DB

--- src/ssisdump.cpp
+++ src/ssisdump.cpp
@@ -10,12 +10,11 @@
 using namespace std;
 
 struct project {
-    project(int64_t id, string_view name, span<const uint8_t> data) : id(id), name(name) {
+    project(int64_t id, span<const uint8_t> data) : id(id) {
         this->data.assign(data.begin(), data.end());
     }
 
     int64_t id;
-    string name;
     vector<uint8_t> data;
     vector<pair<string, vector<uint8_t>>> files;
 };
@@ -78,63 +77,76 @@ static void extract_zip_files(span<const uint8_t> zip, vector<pair<string, vecto
 static void dump_ssis(string_view db_server, string_view db_username, string_view db_password) {
     vector<project> projs;
 
-    tds::tds tds(db_server, db_username, db_password, "ssisdump", "SSISDB");
-
     {
-        tds::query sq(tds, "SELECT internal.get_encryption_algorithm()");
+        tds::tds tds(db_server, db_username, db_password, "ssisdump", "SSISDB");
 
-        if (!sq.fetch_row())
-            throw runtime_error("SSISDB.internal.get_encryption_algorithm returned no value.");
+        {
+            tds::query sq(tds, "SELECT internal.get_encryption_algorithm()");
 
-        auto s = (string)sq[0];
+            if (!sq.fetch_row())
+                throw runtime_error("SSISDB.internal.get_encryption_algorithm returned no value.");
 
-        if (s != "AES_256")
-            throw runtime_error("Unknown encryption algorithm " + s + ".");
-    }
+            auto s = (string)sq[0];
 
-    // FIXME - folders
+            if (s != "AES_256")
+                throw runtime_error("Unknown encryption algorithm " + s + ".");
+        }
 
-    {
-        tds::query sq(tds, R"(SELECT projects.project_id,
-    projects.name,
+        {
+            tds::query sq(tds, R"(SELECT projects.project_id,
     object_versions.object_data
 FROM catalog.projects
 JOIN internal.object_versions ON object_versions.object_id = projects.project_id AND
     object_versions.object_version_lsn = projects.object_version_lsn)");
 
-        while (sq.fetch_row()) {
-            projs.emplace_back((int64_t)sq[0], (string)sq[1], sq[2].val);
+            while (sq.fetch_row()) {
+                projs.emplace_back((int64_t)sq[0], sq[1].val);
+            }
+        }
+
+        for (auto& p : projs) {
+            vector<uint8_t> key;
+            vector<uint8_t> iv;
+            AES_ctx ctx;
+
+            const auto& key_name = "MS_Enckey_Proj_"s + to_string(p.id);
+
+            tds.run(tds::no_check{"OPEN SYMMETRIC KEY " + key_name + " DECRYPTION BY CERTIFICATE MS_Cert_Proj_" + to_string(p.id)});
+
+            {
+                tds::query sq(tds, "SELECT DECRYPTBYKEY([key]), DECRYPTBYKEY(IV) FROM internal.catalog_encryption_keys WHERE key_name = ?", key_name);
+
+                if (!sq.fetch_row())
+                    throw runtime_error("Could not find key " + key_name + " in SSISDB.internal.catalog_encryption_keys.");
+
+                key.assign(sq[0].val.begin(), sq[0].val.end());
+                iv.assign(sq[1].val.begin(), sq[1].val.end());
+            }
+
+            tds.run(tds::no_check{"CLOSE SYMMETRIC KEY " + key_name});
+
+            AES256_init_ctx_iv(&ctx, key.data(), iv.data());
+            AES256_CBC_decrypt_buffer(&ctx, p.data.data(), p.data.size());
+
+            extract_zip_files(p.data, p.files);
         }
     }
 
-    for (auto& p : projs) {
-        vector<uint8_t> key;
-        vector<uint8_t> iv;
-        AES_ctx ctx;
+    tds::tds tds(db_server, db_username, db_password, "ssisdump");
 
-        const auto& key_name = "MS_Enckey_Proj_"s + to_string(p.id);
+    {
+        tds::trans trans(tds);
 
-        tds.run(tds::no_check{"OPEN SYMMETRIC KEY " + key_name + " DECRYPTION BY CERTIFICATE MS_Cert_Proj_" + to_string(p.id)});
+        tds.run("TRUNCATE TABLE mharmstone.ssis_files");
 
-        {
-            tds::query sq(tds, "SELECT DECRYPTBYKEY([key]), DECRYPTBYKEY(IV) FROM internal.catalog_encryption_keys WHERE key_name = ?", key_name);
-
-            if (!sq.fetch_row())
-                throw runtime_error("Could not find key " + key_name + " in SSISDB.internal.catalog_encryption_keys.");
-
-            key.assign(sq[0].val.begin(), sq[0].val.end());
-            iv.assign(sq[1].val.begin(), sq[1].val.end());
+        for (const auto& p : projs) {
+            for (const auto& f: p.files) {
+                tds.run("INSERT INTO mharmstone.ssis_files(project, name, data) VALUES(?, ?, ?)", p.id, f.first, f.second);
+            }
         }
 
-        tds.run(tds::no_check{"CLOSE SYMMETRIC KEY " + key_name});
-
-        AES256_init_ctx_iv(&ctx, key.data(), iv.data());
-        AES256_CBC_decrypt_buffer(&ctx, p.data.data(), p.data.size());
-
-        extract_zip_files(p.data, p.files);
+        trans.commit();
     }
-
-    // FIXME - Git
 }
 
 int main(int argc, char* argv[]) {
