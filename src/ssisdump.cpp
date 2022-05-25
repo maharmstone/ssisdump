c1d4eed2862822a690794386962c20012c94706f
Mark Harmstone <mark@harmstone.com>
2022-05-25 11:53:47 +0100

    add dump_ssis

--- src/ssisdump.cpp
+++ src/ssisdump.cpp
@@ -1,12 +1,19 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <tdscpp.h>
 
 using namespace std;
 
+static void dump_ssis(const string& db_server, string_view db_username, string_view db_password) {
+    tds::tds tds(db_server, db_username, db_password, "ssisdump");
+
+    // FIXME
+}
+
 int main(int argc, char* argv[]) {
     vector<string> args;
-    string db_server;
+    string db_server, db_username, db_password;
 
     args.reserve(argc);
 
@@ -18,11 +25,22 @@ int main(int argc, char* argv[]) {
         db_server = args[1];
 
     if (db_server.empty()) {
-        cout << "Usage: ssisdump <server>" << endl;
+        cerr << "Usage: ssisdump <server>" << endl;
         return 1;
     }
 
-    // FIXME
+    if (getenv("DB_USERNAME"))
+        db_username = getenv("DB_USERNAME");
+
+    if (getenv("DB_PASSWORD"))
+        db_password = getenv("DB_PASSWORD");
+
+    try {
+        dump_ssis(db_server, db_username, db_password);
+    } catch (const exception& e) {
+        cerr << e.what() << endl;
+        return 1;
+    }
 
     return 0;
 }
