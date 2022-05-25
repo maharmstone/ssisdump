d42210df2c0fd445d5f7fcb943f3212cd00ab954
Mark Harmstone <mark@harmstone.com>
2022-05-25 12:17:58 +0100

    dump_ssis: get project data

--- src/ssisdump.cpp
+++ src/ssisdump.cpp
@@ -1,14 +1,49 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <span>
 #include <tdscpp.h>
 
 using namespace std;
 
+struct project {
+    project(int64_t id, string_view name, span<const uint8_t> enc) : id(id), name(name) {
+        this->enc.assign(enc.begin(), enc.end());
+    }
+
+    int64_t id;
+    string name;
+    vector<uint8_t> enc;
+};
+
 static void dump_ssis(const string& db_server, string_view db_username, string_view db_password) {
+    vector<project> projs;
+
     tds::tds tds(db_server, db_username, db_password, "ssisdump");
 
-    // FIXME
+    // FIXME - get SSISDB.internal.get_encryption_algorithm value
+
+    // FIXME - folders
+
+    {
+        tds::query sq(tds, R"(SELECT projects.project_id,
+    projects.name,
+    object_versions.object_data
+FROM SSISDB.catalog.projects
+JOIN SSISDB.internal.object_versions ON object_versions.object_id = projects.project_id AND
+    object_versions.object_version_lsn = projects.object_version_lsn)");
+
+        while (sq.fetch_row()) {
+            projs.emplace_back((int64_t)sq[0], (string)sq[1], sq[2].val);
+        }
+    }
+
+    // FIXME - loop through projects
+    // FIXME - open symmetric key
+    // FIXME - get key and IV from SSISDB.internal.catalog_encryption_keys
+    // FIXME - close symmetric key
+
+    // FIXME - Git
 }
 
 int main(int argc, char* argv[]) {
