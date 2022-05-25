41201ab9ddd6bcec381e0b6bd8db9d2d16080883
Mark Harmstone <mark@harmstone.com>
2022-05-25 11:48:35 +0100

    main: get server name from command line

--- src/ssisdump.cpp
+++ src/ssisdump.cpp
@@ -1,4 +1,27 @@
-int main() {
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    vector<string> args;
+    string db_server;
+
+    args.reserve(argc);
+
+    for (int i = 0; i < argc; i++) {
+        args.emplace_back(argv[i]);
+    }
+
+    if (argc >= 2)
+        db_server = args[1];
+
+    if (db_server.empty()) {
+        cout << "Usage: ssisdump <server>" << endl;
+        return 1;
+    }
+
     // FIXME
 
     return 0;
